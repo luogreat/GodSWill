770cd782ffdfb85f94550533d4cd18ce78cf4ba2
luogreat <luow25@sanygroup.cn>
2017-11-24 09:00:13 +0800

    Initial commit

---
+++ src/test/java/com/godswill/GodswillApplicationTests.java
@@ -0,0 +1,16 @@
+package com.godswill;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GodswillApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
